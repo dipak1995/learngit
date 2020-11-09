f45c66cc042581e9c11e2b15d651ea8fbdf269c4
dipak1995 <439900544@qq.com>
2020-11-09 11:06:43 +0800

    i am test-djy2

---
+++ src/main/java/com/ptt/config/Test.java
@@ -0,0 +1,9 @@
+package com.ptt.config;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+    }
+}
